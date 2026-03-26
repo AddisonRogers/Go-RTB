9eed72bb6db18b02036bbb5fa55e456ae3318ced
AddisonRogers <addisonrogers@protonmail.com>
2026-03-26 00:26:01 +0000

    sleep. But dockercompsoe init and some work on a worker service

---
+++ othernotes.md
@@ -0,0 +1,9 @@
+# Challenges
+
+## Throughput computation on expiry
+So the issue was that I need to approve/deny the bidrequest in the banker based off of the computed amount of spend / 10 minutes.
+This is obviously an issue as I will need to either compute the property each time a bid is sent or calculate it different time.
+The way I decided to do it is obviously calculate + change when there has been a clear/approve operation but how do I handle expiries
+I considered using a goroutine to handle things in the background but ultimately this went against my microservice plan.
+So I instead wanted to use a queue + a worker of some kind. The next question was how do I make it so that I dont have the worker polling 
+constantly the same db and having a poor scale up. So I looked up about redis and found keyspace notifications which solved a big part of this.
--- worker/main.go
+++ worker/main.go
@@ -1,7 +1,9 @@
 package worker
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/AddisonRogers/Go-RTB/shared"
 	"github.com/redis/go-redis/v9"
@@ -30,14 +32,44 @@ func main() {
 		}
 	}(rdb)
 
+	ctx := context.Background()
+	pubsub := rdb.PSubscribe(ctx, "__keyevent@0__:expired")
+	defer func(pubsub *redis.PubSub) {
+		err := pubsub.Close()
+		if err != nil {
+			fmt.Println("Error closing redis pubsub", err)
+		}
+	}(pubsub)
+
 	redisAdapter := shared.NewRedisAdapter(rdb)
 
-	// Inject the adapter (which implements Cacher) into our service
 	svc := NewWorkerService(redisAdapter)
 
+	ch := pubsub.Channel()
+
+	for msg := range ch {
+		fmt.Println("Received message:", msg.Channel, msg.Payload)
+		fmt.Println("expired key:", msg.Payload)
+		svc.checkThroughput(ctx, msg.Payload)
+	}
 }
 
 // todo - run a loop that checks for expired keys and deletes them
-func (s *DependencyService) checkThroughput(id string) {
+func (s *DependencyService) checkThroughput(ctx context.Context, key string) {
+	if key == "" {
+		return
+	}
+
+	values, err := s.cache.FindKeysByValue(ctx, fmt.Sprintf("%s:campaign:*", id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	held, err := s.cache.FindKeysByValue(ctx, fmt.Sprintf("%s:hold:*", id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 }
